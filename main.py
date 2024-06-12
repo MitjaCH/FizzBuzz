65a97510b5cd617c71e5b0ae4da28e4ea07ed868
Mitja Kurath <mitja.kurath@bbcag.ch>
2024-06-12 08:48:20 +0200

    [UPDATE] Changed File names of .java and .py + added c++ file

--- main.py
+++
@@ -1,9 +0,0 @@
-for i in range(100):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Buzz")
-    elif i % 5 == 0:
-        print("Fizz")
-    else:
-        print(i)
