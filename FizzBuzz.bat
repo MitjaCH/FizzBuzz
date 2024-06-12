85f5ef21f6a1b66a1837b5c098db0eefd02c0c39
nxkzty <Leon.hoefferer@gmail.com>
2024-06-12 09:11:52 +0200

    worked on batchfile

--- FizzBuzz.bat
+++ FizzBuzz.bat
@@ -1,27 +1,21 @@
 @echo off
 
-for /l %%i in (1, 1, 100) do (
-    if !errorlevel! equ 0 (
-        if %%i mod 3 equ 0 (
-            echo Fizz
-            set /a errorlevel=1
-        ) else if %%i mod 5 equ 0 (
-            echo Buzz
-            set /a errorlevel=1
+for /l %%i in (1,1,100) do (
+    set /a fizz=%%i %% 3
+    set /a buzz=%%i %% 5
+    if %fizz%==0 (
+        if %buzz%==0 (
+            echo FizzBuzz
         ) else (
-            echo %%i
-            set /a errorlevel=0
+            echo Fizz
         )
     ) else (
-        if %%i mod 3 equ 0 (
-            echo -
-            set /a errorlevel=0
-        ) else if %%i mod 5 equ 0 (
-            echo -
-            set /a errorlevel=0
+        if %buzz%==0 (
+            echo Buzz
         ) else (
             echo %%i
-            set /a errorlevel=1
         )
     )
 )
+
+pause
